5f285188409434ac296d520a8f0b187a5c0b5867
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-01-27 12:59:41 +0600

    feat: add basic event handler

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -43,13 +43,32 @@ function BookList() {
 }
 
 const Book = ({ img, title, author }) => {
+  const clickHandler = () => {
+    console.log("btn was clicked!");
+  };
+  const clickComplexBtn = () => {
+    console.log(author);
+  };
   return (
     <section className='book'>
       <img src={img} alt='book' />
-      <h4 style={{ color: "#4360a2", fontSize: "1.2rem", paddingTop: "1rem" }}>
+      <h4
+        onClick={() => {
+          console.log(title);
+        }}
+        style={{ color: "#4360a2", fontSize: "1.2rem", paddingTop: "1rem" }}
+      >
         {title}
       </h4>
       <h3>{author}</h3>
+      <button onClick={clickHandler}>click me!</button>
+      <button
+        onClick={() => {
+          clickComplexBtn(author);
+        }}
+      >
+        complex btn!
+      </button>
     </section>
   );
 };
