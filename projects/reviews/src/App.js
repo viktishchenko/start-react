3394047eae4014fa74502ecb27a154a67d4e99b5
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-02-03 12:58:49 +0600

    init: start reviews project

---
+++ projects/reviews/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Review from "./Review";
+
+const App = () => {
+  return <h2>reviews project</h2>;
+};
+
+export default App;
---
+++ projects/reviews/src/Review.js
@@ -0,0 +1,9 @@
+import React from "react";
+import people from "./data";
+// import { FaChevronLeft, FaChevronRight, FaQuoterRight } from "react-icons/fa";
+
+const Review = () => {
+  return <h2>review component</h2>;
+};
+
+export default Review;
