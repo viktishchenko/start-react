500cf349e3a758ddf3ab15d633f874854bb778f0
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-02-04 15:01:24 +0600

    add accordion project

--- projects/accordion/README.md
+++ projects/accordion/README.md
@@ -1,3 +1,5 @@
 # accordeon project
 
 ### 04.02.2022 13:02
+
+![Screenshot 2022-02-04 at 14-19-20 React App](./public/Screenshot-1.png)
--- projects/accordion/src/App.js
+++ projects/accordion/src/App.js
@@ -3,7 +3,20 @@ import data from "./data";
 import SingleQuestion from "./Question";
 
 const App = () => {
-  return <h2>accordeon project</h2>;
+  const [question] = useState(data);
+
+  return (
+    <main>
+      <div className='container'>
+        <h3>questions and answers about smth</h3>
+        <section className='info'>
+          {question.map((el) => {
+            return <SingleQuestion key={el.id} {...el} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
 };
 
 export default App;
