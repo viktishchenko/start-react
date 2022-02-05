889a033f1fc7d838844b054f2aa72b2f6c7e7eff
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-02-05 14:13:27 +0600

    init tabs project

---
+++ projects/tabs/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+
+const url = "https://course-api.com/react-tabs-project";
+
+const App = () => {
+  return <h2>tabs project</h2>;
+};
+
+export default App;
