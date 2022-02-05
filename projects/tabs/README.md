55ab712646e340882b018bef12778eda7ec26e96
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-02-05 20:19:43 +0600

    feat: add tabs project

--- projects/tabs/README.md
+++ projects/tabs/README.md
@@ -1,3 +1,5 @@
 # tabs project
 
 ### 05.02.2022 14:14
+
+![Screenshot 2022-02-05 at 20-16-24 React App](./public/Screenshot-1.png)
--- projects/tabs/src/App.js
+++ projects/tabs/src/App.js
@@ -4,7 +4,69 @@ import { FaAngleDoubleRight } from "react-icons/fa";
 const url = "https://course-api.com/react-tabs-project";
 
 const App = () => {
-  return <h2>tabs project</h2>;
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(1);
+
+  const fetchJob = async () => {
+    const res = await fetch(url);
+    const newJob = await res.json();
+    setJobs(newJob);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJob();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className='loading section'>
+        <h2>loading...</h2>
+      </section>
+    );
+  }
+
+  const { company, dates, title, duties } = jobs[value];
+
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>expierence</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='jobs-center'>
+        <div className='btn-container'>
+          {jobs.map((el, idx) => {
+            return (
+              <button
+                className={`job-btn ${idx === value && "active-btn"}`}
+                onClick={() => {
+                  setValue(idx);
+                }}
+                key={el.id}
+              >
+                {el.company}
+              </button>
+            );
+          })}
+        </div>
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-title'>{dates}</p>
+          {duties.map((el, idx) => {
+            return (
+              <div className='job-desc' key={idx}>
+                <FaAngleDoubleRight className='job-icon' />
+                <p>{el}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default App;
