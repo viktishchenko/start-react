fa9d38bf1c903dccb29732ac7a8aba38863d3f26
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-02-07 05:28:16 +0600

    init: slider project

---
+++ projects/slider/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+
+const url = "https://course-api.com/react-tabs-project";
+
+const App = () => {
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>experience</h2>
+      </div>
+    </section>
+  );
+};
+
+export default App;
