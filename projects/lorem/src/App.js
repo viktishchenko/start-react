ffebe1311718db856fc186e5b0eba3781f30c68f
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-02-07 16:50:33 +0600

    init: start lorem project

---
+++ projects/lorem/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import data from "./data";
+
+const App = () => {
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>lorem app</h2>
+      </div>
+    </section>
+  );
+};
+
+export default App;
