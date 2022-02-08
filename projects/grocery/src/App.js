7ab473983184187f45ccce318ac8dc82e126a550
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-02-08 20:55:44 +0600

    feat: add basic structure

--- projects/grocery/src/App.js
+++ projects/grocery/src/App.js
@@ -3,7 +3,43 @@ import List from "./List";
 import Alert from "./Alert";
 
 function App() {
-  return <h2>grocery bud setup</h2>;
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("name :>> ", name);
+  };
+
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+          <input
+            className='grocery'
+            type='text'
+            placeholder='e.g. eggs'
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <button className='submit-btn' type='submit'>
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+      <div className='grocery-container'>
+        <List />
+        <button className='clear-btn'>clear items</button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
