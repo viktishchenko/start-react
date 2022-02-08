18773169345e450744a3135c75813d3d18efd9ff
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-02-08 21:19:25 +0600

    feat: add list component w display logic

--- projects/grocery/src/App.js
+++ projects/grocery/src/App.js
@@ -11,7 +11,16 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("name :>> ", name);
+    if (!name) {
+      // display alert
+    } else if (name && isEditing) {
+      // deal with edit mode
+    } else {
+      //show alert
+      const newItem = { id: Date.now().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
   };
 
   return (
@@ -34,10 +43,12 @@ function App() {
           </button>
         </div>
       </form>
-      <div className='grocery-container'>
-        <List />
-        <button className='clear-btn'>clear items</button>
-      </div>
+      {list.length > 0 && (
+        <div className='grocery-container'>
+          <List items={list} />
+          <button className='clear-btn'>clear items</button>
+        </div>
+      )}
     </section>
   );
 }
