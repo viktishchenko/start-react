7f39ac91be34b94fc2b11f727e1451787edcd8d3
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-02-10 12:48:30 +0600

    add context exp custom context hook

--- projects/sidebar-modal/src/Home.js
+++ projects/sidebar-modal/src/Home.js
@@ -2,7 +2,23 @@ import React, { useContext } from "react";
 import { FaBars } from "react-icons/fa";
 
 const Home = () => {
-  return <h2>home component</h2>;
+  /**
+   *  get context data from context.js
+   *  import { useContext } from "react";
+   *  import { AppContext } from "./context";
+   *
+   *  const ourData = useContext(AppContext);
+   *  console.log("ourData :>> ", ourData);
+   */
+
+  return (
+    <main>
+      <button className='sidebar-toggle'>
+        <FaBars />
+      </button>
+      <button className='btn'>show modal</button>
+    </main>
+  );
 };
 
 export default Home;
--- projects/sidebar-modal/src/Sidebar.js
+++ projects/sidebar-modal/src/Sidebar.js
@@ -4,7 +4,47 @@ import { FaTimes } from "react-icons/fa";
 import { social, links } from "./data";
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>;
+  /**
+   *  context data from hook
+   *  import { useGlobalContext } from "./context";
+   *
+   *  const contextData = useGlobalContext();
+   *  console.log("contextData :>> ", contextData);
+   */
+
+  return (
+    <aside className={"sidebar"}>
+      <div className='sidebar-header'>
+        <img src={logo} alt='logo' className='logo' />
+        <button className='close-btn'>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className='links'>
+        {links.map((item) => {
+          const { id, url, text, icon } = item;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className='social-icons'>
+        {social.map((item) => {
+          const { id, url, icon } = item;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
 };
 
 export default Sidebar;
