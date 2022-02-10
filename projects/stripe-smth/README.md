1e1a0ff34f9100ed6ad5ec6c725206cb664c222b
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-02-10 21:37:50 +0600

    info: finish global context codition rendering task

--- projects/stripe-smth/README.md
+++ projects/stripe-smth/README.md
@@ -1,3 +1,5 @@
 # stripe-smth project
 
 ### 10.02.2022
+
+![Screenshot 2022-02-10 at 21-30-12 React App](./public/Screenshot-1.png)
--- projects/stripe-smth/src/context.js
+++ projects/stripe-smth/src/context.js
@@ -8,7 +8,9 @@ const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [location, setLocation] = useState({});
+  const [page, setPage] = useState({ page: "", links: [] });
 
   const openSidebar = () => {
     setIsSidebarOpen(true);
@@ -16,7 +18,10 @@ export const AppProvider = ({ children }) => {
   const closeSidebar = () => {
     setIsSidebarOpen(false);
   };
-  const openSubmenu = () => {
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
     setIsSubmenuOpen(true);
   };
   const closeSubmenu = () => {
@@ -32,6 +37,8 @@ export const AppProvider = ({ children }) => {
         openSubmenu,
         closeSidebar,
         openSidebar,
+        location,
+        page,
       }}
     >
       {children}
