8411beb242c53e93076e9a40eacca12213df5272
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-02-12 11:00:10 +0600

    feat: add useReducer task

--- projects/cart/README.md
+++ projects/cart/README.md
@@ -1,3 +1,5 @@
 # cart project
 
 ### 10.02.2022 21:53
+
+![Screenshot 2022-02-12 at 10-45-05 React App](./public/Screenshot-1.png)
--- projects/cart/src/context.js
+++ projects/cart/src/context.js
@@ -3,6 +3,7 @@ import cartItems from "./data";
 import reducer from "./reducer";
 
 const url = "https://course-api.com/react-useReducer-cart-project";
+
 const AppContext = React.createContext();
 
 const initialState = {
@@ -15,10 +16,54 @@ const initialState = {
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
+
+  const remove = (id) => {
+    dispatch({ type: "REMOVE", payload: id });
+  };
+
+  const increase = (id) => {
+    dispatch({ type: "INCREASE", payload: id });
+  };
+
+  const decrease = (id) => {
+    dispatch({ type: "DECREASE", payload: id });
+  };
+
+  useEffect(() => {
+    dispatch({ type: "GET_TOTALS" });
+  }, [state.cart]);
+
+  const fetchData = async () => {
+    dispatch({ type: "LOADING" });
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({ type: "DISPLAY_ITEMS", payload: cart });
+  };
+
+  /**
+   * apset about dri f
+   */
+
+  const toggleAmount = (id, type) => {
+    dispatch({ type: "TOGGLE_AMOUNT", payload: { id, type } });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <AppContext.Provider
       value={{
         ...state,
+        clearCart,
+        remove,
+        increase,
+        decrease,
+        toggleAmount,
       }}
     >
       {children}
