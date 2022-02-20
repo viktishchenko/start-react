bda949f39b8cf8b2d400ed8c4412a2d451e28ecf
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-02-20 15:28:42 +0600

    feat: add fetch route context project

--- projects/movie/README.md
+++ projects/movie/README.md
@@ -1,3 +1,5 @@
 # movie db project
 
 ### 20.02.2022 07:02
+
+![Screenshot 2022-02-20 at 15-25-13 React App](./public/Screenshot-1.png)
--- projects/movie/src/context.js
+++ projects/movie/src/context.js
@@ -1,10 +1,19 @@
 import React, { useState, useContext, useEffect } from "react";
-// make sure to use https
-export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
+import useFetch from "./useFetch";
+
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_DB_ACCESS_KEY}`;
+
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>;
+  const [query, setQuery] = useState("batman");
+  const { isLoading, error, data: movies } = useFetch(`&s=${query}`);
+
+  return (
+    <AppContext.Provider value={{ isLoading, error, movies, query, setQuery }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 // make sure use
 export const useGlobalContext = () => {
