6f454ff4e4125fd3b1c75025f13548f33bd0edb4
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-02-24 22:25:07 +0600

    feat: add remove stories functionalyty

--- projects/hack-news/src/reducer.js
+++ projects/hack-news/src/reducer.js
@@ -19,6 +19,12 @@ const reducer = (state, action) => {
         nbPages: action.payload.nbPages,
       };
       break;
+    case REMOVE_STORY:
+      return {
+        ...state,
+        hits: state.hits.filter((story) => story.objectID !== action.payload),
+      };
+      break;
 
     default:
       throw new Error(`no matching '${action.type}' action type`);
